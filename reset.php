acc4e8f5db9b772f5554b510c15390e567cb6166
Spesiel <luckypenny.blossom+github@gmail.com>
2017-02-17 18:50:23 -0600

    Resets the tables. Used in tests

---
+++ reset.php
@@ -0,0 +1,44 @@
+<?php namespace Completionistv2;
+
+use \PDO;
+
+const TABLES = array(
+    "bookmarks" => false,
+    "sessions"  => true,
+    "games"     => true,
+    "users"     => true
+);
+
+require_once $_SERVER["DOCUMENT_ROOT"]."/lib/CompletionistException.php";
+
+foreach (TABLES as $key => $value) {
+    plop("SET foreign_key_checks = 0; TRUNCATE TABLE $key; SET foreign_key_checks = 1;", $key);
+
+    if ($value===true) {
+        plop("ALTER TABLE $key auto_increment = 1;", $key);
+    }
+}
+
+function plop($query, $key)
+{
+    $connection = new PDO(
+        "mysql:host=localhost;dbname=completionistv2;charset=utf8mb4",
+        "completionistv2",
+        "default",
+        array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        )
+    );
+
+    try {
+        printf("<hr/>$query:");
+        $connection->beginTransaction();
+        $statement = $connection->prepare($query);
+        $result = $statement->execute();
+        var_dump($result);
+        $connection->commit();
+    } catch (\PDOException $e) {
+        $connection->rollBack();
+        new CompletionistException($e->getMessage());
+    }
+}
