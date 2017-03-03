ce7771f906da3fcf1a8ba353d20e0644e1f71241
Spesiel <luckypenny.blossom+github@gmail.com>
2017-03-03 03:15:59 -0600

    Friends: Changed status working abilities

--- lib/Dao/Friends.php
+++ lib/Dao/Friends.php
@@ -2,11 +2,23 @@
 
 class Friends
 {
+    const STATUS = array(
+        127 => "accepted",
+         63 => "requested",
+          1 => "blocked"
+    );
+
     public static function select($columns = array("*"), $filters = array())
     {
         require_once $_SERVER["DOCUMENT_ROOT"]."\lib\Dao\Database.php";
 
-        return Database::select("friends", $columns, $filters);
+        $result = Database::select("friends", $columns, $filters);
+
+        foreach ($result->rows as $row) {
+            $row->status = self::STATUS[($row->status)];
+        }
+
+        return $result;
     }
 
     public static function getRequests($userid)
@@ -48,8 +60,8 @@ class Friends
 
         return Database::insert(
             "friends",
-            array("userid","friendid"),
-            array($userid,$friendid)
+            array("userid","friendid","status"),
+            array($userid,$friendid,array_search("requested", self::STATUS))
         );
     }
 
@@ -60,8 +72,20 @@ class Friends
         return Database::update(
             "friends",
             array("status"),
-            array(1),
+            array(array_search("accepted", self::STATUS)),
             array("userid=$friendid","friendid=$userid")
+        );
+    }
+
+    public static function block($userid, $friendid)
+    {
+        require_once $_SERVER["DOCUMENT_ROOT"]."\lib\Dao\Database.php";
+
+        return Database::update(
+            "friends",
+            array("status"),
+            array(array_search("blocked", self::STATUS)),
+            array("userid=$userid","friendid=$friendid")
         );
     }
 
