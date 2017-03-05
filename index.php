bfbb07835d6ec8346e5ec41c14094d6d1385cf52
Spesiel <luckypenny.blossom+github@gmail.com>
2017-03-04 22:31:36 -0600

    Modifications due to testing

--- index.php
+++ index.php
@@ -11,27 +11,22 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/SessionsTests.php";
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/GamesTests.php";
 
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/BookmarksTests.php";
-
 printf("<hr/><hr/><h1>Reset</h1>");
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/reset.php";
 
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/FriendsTests.php";
-
 printf("<hr/><hr/><h1>Reset</h1>");
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/reset.php";
 
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/MessagesTests.php";
-
 printf("<hr/><hr/><h1>Reset</h1>");
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/reset.php";
 
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/RolesTests.php";
-
 printf("<hr/><hr/><h1>Reset</h1>");
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/reset.php";
 ***********************************************/
 
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/GamesTests.php";
-
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/FriendsTests.php";
 printf("<hr/><hr/><h1>Reset</h1>");
 require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/reset.php";
