2cffe917462f7a6dc5887c457f704848b7609408
Spesiel <luckypenny.blossom+github@gmail.com>
2017-03-12 10:25:26 -0500

    Tests: Helper/Dao done.

--- .tests/Run.php
+++ .tests/Run.php
@@ -5,26 +5,29 @@ require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/TestFunctions.php";
 printf("<h1>Tests</h1><hr/>");
 
 /***********************************************/
-require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
+printf("<h2>Helper</h2><hr/>");
 
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/PasswordHelperTests.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/TokenHelperTests.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Helper/PasswordHelperTests.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Helper/TokenHelperTests.php";
 
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/UsersTests.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/SessionsTests.php";
-require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
-
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/GamesTests.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/CompletionTests.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/BookmarksTests.php";
-require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
-
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/FriendsTests.php";
-require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
+/***********************************************/
+printf("<h2>Dao</h2><hr/>");
 
 require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
 
-require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/RolesTests.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Dao/UsersTests.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Dao/SessionsTests.php";
+require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
+
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Dao/GamesTests.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Dao/CompletionTests.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Dao/BookmarksTests.php";
+require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
+
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Dao/FriendsTests.php";
+require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
+
+require_once $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/Dao/RolesTests.php";
 require $_SERVER["DOCUMENT_ROOT"]."/.tests/Units/reset.php";
 /***********************************************/
 
